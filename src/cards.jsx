19fdfe4d07328ee55bf44d84a738313f1f0feaf0
ElKanekas <ovanegasmira@gmail.com>
2024-11-16 21:47:39 -0500

    Actualizacion de las cartas

--- src/cards.jsx
+++ src/cards.jsx
@@ -1,28 +1,42 @@
 import React, { useState } from "react";
 import "./cards.css";
 
-export function VinoCard({ nombre_vino, imagen, lo_compraste }) {
-  const [comprar, setComprando] = useState(lo_compraste);
-  const buyText = comprar ? "Eliminar del carrito" : "Añadir al carrito";
+export function VinoCard({ nombre_vino, imagen, button='Añadir Al Carrito' }) {
+  const [Añadir, setComprando] = useState(false);
 
   const handleClick = () => {
-    setComprando(!comprar);
+    setComprando(!Añadir);
   };
 
   return (
-    <div className="Cards">
-      <div className="VinoCard-Container">
-        <div className="VinoCard-Img">
-          <img src={imagen} alt="" className="VinoCard-Img" />
-          <h3>{nombre_vino}</h3>
-        </div>
 
-        <div className="">
-          <button className="VinoCard-Button" onClick={handleClick}>
-            <span>{buyText}</span>
+    <div className="cards">
+      <div className="card">
+        <div className="card-img">
+          <img src={imagen} alt=""/>
+          <a className="filtro">
+            <div className="filtro-content">
+              <p>INFORMACION</p>
+              <button>Ver Más</button>
+            </div>
+          </a>
+        </div>
+        <div className="card-content">
+          <h3>
+            TITULO
+          </h3>
+          <p>
+            $PRECIO$
+          </p>
+          <button
+            onClick={handleClick}
+            className={Añadir ? 'boton-Añadido' : 'boton-Añadir'}
+          >
+            {Añadir ? 'Quitar Del Carrito' : button}
           </button>
         </div>
       </div>
     </div>
+
   );
 }
